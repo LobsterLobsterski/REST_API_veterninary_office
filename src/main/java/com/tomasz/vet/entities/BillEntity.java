b860427029344ba58ae73725caba359828fb27cf
Tomasz Potoczko <tomasz.spezik@gmail.com>
2023-11-11 11:48:11 +0100

    New: - added the bill and procedure entities and created a many-to-many relationship between them

--- src/main/java/com/tomasz/vet/entities/BillEntity.java
+++ src/main/java/com/tomasz/vet/entities/BillEntity.java
@@ -1,4 +1,43 @@
 package com.tomasz.vet.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="bills")
 public class BillEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bill_id_seq")
+    @Column(name="bill_id")
+    private Long id;
+
+    private Date issueDate;
+
+    @ManyToMany
+    @JoinTable(name="bill_procedure",
+            joinColumns= {
+                @JoinColumn(name = "bill_id")
+            },
+            inverseJoinColumns={
+                @JoinColumn(name="procedure_id")
+            }
+    )
+    private Set<ProcedureEntity> proceduresBilled = new HashSet<>();
+
+    public float getPrice(){
+        System.out.println(proceduresBilled);
+        return 0;
+    }
+
 }
--- src/main/java/com/tomasz/vet/entities/ProcedureEntity.java
+++ src/main/java/com/tomasz/vet/entities/ProcedureEntity.java
@@ -1,4 +1,30 @@
 package com.tomasz.vet.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name="procedures")
 public class ProcedureEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "procedure_id_seq")
+    @Column(name="procedure_id")
+    private Long id;
+
+    private String name;
+    private float cost;
+    private String comments;
+
+    @ManyToMany(mappedBy = "proceduresBilled")
+    private Set<BillEntity> onBills = new HashSet<>();
 }
